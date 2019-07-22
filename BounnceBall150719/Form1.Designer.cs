20c6eb869ccaaefff14615a2d9d0742af9acefc5
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-07-23 02:07:34 +0300

    Add files via upload

---
+++ BounnceBall150719/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BounnceBall150719
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "bounce";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 414);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(156, 411);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BounnceBall150719.Properties.Resources.pokaball4;
+            this.pictureBox1.Location = new System.Drawing.Point(192, 141);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(475, 393);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(499, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ BounnceBall150719/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BounnceBall150719
+{
+    public partial class Form1 : Form
+    {
+        int dx = 2;
+        int dy = 2;
+        bool stop = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)// start
+        {
+
+            Task.Run(() =>
+            {
+                stop = false;
+                while (stop == false)
+                {
+
+                    Action a = () =>
+                    {
+                        if (pictureBox1 != null)
+                        {
+                            pictureBox1.Location = new Point(pictureBox1.Location.X + dx, pictureBox1.Location.Y + dy);
+                        }
+                        else
+                        {
+
+                        }
+                    };
+                       
+                    pictureBox1.BeginInvoke(a);
+
+                    if (pictureBox1.Location.X <= 0)
+                        dx = 2;
+
+                    if (pictureBox1.Location.Y <= 0)
+                        dy = 2;
+
+                    if (pictureBox1.Location.X >= panel1.Width - pictureBox1.Width)
+                        dx = -2;
+                    if (pictureBox1.Location.Y >= panel1.Height - pictureBox1.Height)
+                        dy = -2;
+
+                    Thread.Sleep(10);
+                }
+            });
+        }
+        private void button2_Click(object sender, EventArgs e) // stop
+        {
+            stop = true;
+        }
+        private void NewBall(PictureBox picture)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e) // add
+        {
+            stop = false;
+            var picture = new PictureBox
+            {
+                Name = "picture box",
+                Location = new Point(173, 105),
+                Image = global::BounnceBall150719.Properties.Resources.pokaball1,
+                SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom,
+                TabIndex = 1,
+                Size = new System.Drawing.Size(136, 130),
+                TabStop = false,
+
+            };
+            picture.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.Controls.Add(picture);
+            Task.Run(() =>
+            {
+
+                while (stop == false)
+                {
+
+                    Action b = () =>
+                 {
+                     { picture.Location = new Point(picture.Location.X + dx, picture.Location.Y + dy); }
+                 };
+                    picture.BeginInvoke(b);
+                    if (picture.Location.X <= 0)
+                        dx = 2;
+                    if (picture.Location.Y <= 0)
+                        dy = 2;
+                    if (picture.Location.X >= panel1.Width - picture.Width)
+                        dx = -2;
+                    if (picture.Location.Y >= panel1.Height - picture.Height)
+                        dy = -2;
+                    Thread.Sleep(10);
+                }
+            });
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //var picture = new PictureBox
+            //{
+            //    Name = "explode",
+            //    Location = new Point(173, 105),
+            //    Image = Image.FromFile(@"C:\Users\USER\Desktop\explode.ping"),
+            //    SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom,
+            //    TabIndex = 1,
+            //    Size = new System.Drawing.Size(136, 130),
+            //    TabStop = false,
+
+            //};
+        }
+    }
+}
